49acbf9be0dad2173fb5aa21d31391ad4cab8e4e
Janaki Kshetri <168868061+jasmee03@users.noreply.github.com>
2024-11-27 21:37:34 +0545

    2nd commit

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-var typed = new typed(".text", {
-    Strings:["Fronted Devloper" , "web Designer", "web Developer"],
+var typed = new typed(".typed.text", {
+    Strings:['Fronted Devloper' , 'Full Stack Devloper', 'web Designer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
 });
+
+//hamburger menu 
+const hamburg = document.querySelector('.hamburg')
+        const cancel = document.querySelector('.cancel')
+        const navbar = document.querySelector('.dropdown');
+
+        hamburg.addEventListener('click', () => {
+            navbar.style.transform = "translateY(0px)"
+            hamburg.style.display = 'none'
+        })
+        cancel.addEventListener('click', () => {
+            navbar.style.transform = "translateY(-300px)"
+            hamburg.style.display = 'block'
+        })
+
+
+
 document.getElementById('moreInfoBtn').addEventListener('click', function() {
     const moreInfo = document.getElementById('moreInfo');
     if (moreInfo.classList.contains('hidden')) {
